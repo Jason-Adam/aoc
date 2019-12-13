a7e3d2b2e487cb7f81df22a443fa51c3cf81dfb1
Jason Adam <jason@myndshft.com>
2019-12-13 16:02:00 -0700

    updated day 3

--- day_03/day_03.go
+++ day_03/day_03.go
@@ -2,30 +2,58 @@ package main
 
 import (
 	"bufio"
+	"encoding/csv"
 	"fmt"
+	"io"
 	"os"
-	"strings"
+	"strconv"
 )
 
-// readInput reads in a txt file and converts to slice of ints.
-func readInput(f string) ([]string, error) {
+// wire struct contains the direction and distance values of a wire.
+type wire struct {
+	direction []string
+	distance  []int
+}
+
+// readInput converts the input file to a slice of wire structs.
+func readInput(f string) ([]wire, error) {
 	file, err := os.Open(f)
 	if err != nil {
 		return nil, err
 	}
-	scanner := bufio.NewScanner(file)
-	for scanner.Scan() {
-		line := scanner.Text()
-		s := strings.Split(line, ",")
-		return s, nil
+	var wires []wire
+	reader := csv.NewReader(bufio.NewReader(file))
+	for {
+		line, err := reader.Read()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			fmt.Println(err)
+		}
+		var d []string
+		var v []int
+		for i, _ := range line {
+			d = append(d, string(line[i][0]))
+			val, err := strconv.Atoi(line[i][1:])
+			if err != nil {
+				fmt.Println(err)
+			}
+			v = append(v, val)
+		}
+		wires = append(wires, wire{
+			direction: d,
+			distance:  v,
+		})
 	}
-	return nil, nil
+	return wires, nil
 }
 
 func main() {
-	f, err := readInput("input.txt")
+	w, err := readInput("input.csv")
 	if err != nil {
 		fmt.Println(err)
 	}
-	fmt.Println(f)
+	fmt.Println(w[0].direction)
+	fmt.Println(w[0].distance)
 }
