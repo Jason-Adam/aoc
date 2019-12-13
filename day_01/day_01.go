3155c21f32d1def6107906b6a0ebbffd6cebf61f
Jason Adam <jason@myndshft.com>
2019-12-13 08:46:30 -0700

    day 1 completed

---
+++ day_01/day_01.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+)
+
+var fuel int64 = 0
+
+func calcFuel(n int64) {
+	reduced := (n / 3) - 2
+	if reduced <= 0 {
+		return
+	}
+	fuel += reduced
+	calcFuel(reduced)
+}
+
+func main() {
+	file, err := os.Open("input.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		line := scanner.Text()
+		num, err := strconv.ParseInt(line, 0, 64)
+		if err != nil {
+			log.Fatal(err)
+		}
+		calcFuel(num)
+	}
+	fmt.Println(fuel)
+}
